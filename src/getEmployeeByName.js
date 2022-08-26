a22b0dab7e96c66e2924731bae1e7448e47160a7
Renanbfreitas <renanbf1992@outlook.com>
2022-08-26 18:05:57 -0300

    Adicionando item 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const { employees } = data;
 
+function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) return {};
+  return employees.find((employee) =>
+    (employees.firstName === employeeName || employees.lastName === employeeName));
+}
+console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
 function isManager(id) {
-  // seu código aqui
+  if (id === stephanieId || id === olaId || id === burlId) {
+    return employees.some((manager) => manager.id === id);
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const result = isManager(managerId);
+  if (result !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((employee) => employee.managers
+    .some((value) => value === managerId))
+    .map((names) => `${names.firstName} ${names.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
