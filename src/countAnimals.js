e1979935078b301e669caa85cbfb6cbef76d104e
Renanbfreitas <renanbf1992@outlook.com>
2022-08-26 18:42:09 -0300

    Adicionando item 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species: animais } = data;
+function countAnimals(animalQuant) {
+  if (animalQuant === undefined) {
+    return animais.reduce((element1, element2) => {
+      const qtdAnimal = element1;
+      qtdAnimal[element2.name] = element2.residents.length;
+      return element1;
+    }, {});
+  } if (animalQuant.sex) {
+    return animais
+      .find((bichos) => (bichos.name === animalQuant.specie)).residents
+      .filter((residem) => residem.sex === animalQuant.sex).length;
+  }
+  return animais.find((ani) => ani.name === animalQuant.specie).residents.length;
 }
 
 module.exports = countAnimals;
