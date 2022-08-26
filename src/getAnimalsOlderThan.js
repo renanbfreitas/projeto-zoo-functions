30b5337d6e5f06d79b72562b7c327e50eb3449e3
Renanbfreitas <renanbf1992@outlook.com>
2022-08-26 17:12:49 -0300

    Adicionando item 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(specie, ageMin) {
+  return data.species
+    .find((element) => element.name === specie).residents
+    .every((element) => element.age >= ageMin);
 }
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
