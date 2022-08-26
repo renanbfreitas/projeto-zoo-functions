eb3350cc6f2651de1ff88f3b906d05e8c5ff8a29
Renanbfreitas <renanbf1992@outlook.com>
2022-08-26 20:06:47 -0300

    Adicionando item 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const numeroVisitante = countEntrants(entrants);
+  const numeroChild = numeroVisitante.child * data.prices.child;
+  const numeroAdult = numeroVisitante.adult * data.prices.adult;
+  const numeroSenior = numeroVisitante.senior * data.prices.senior;
+  return numeroChild + numeroAdult + numeroSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
