cb214f4f56551692fa42c447e798880c6d6f92c0
Renanbfreitas <renanbf1992@outlook.com>
2022-08-26 16:18:20 -0300

    Adicionando item 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((element) => ids.includes(element.id));
 }
 
+console.log(getSpeciesByIds(data));
 module.exports = getSpeciesByIds;
