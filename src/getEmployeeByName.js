671066577a629d38f00e4afe6868738573fb4e86
Renanbfreitas <renanbf1992@outlook.com>
2022-08-27 11:21:51 -0300

    Adicionando item 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -4,8 +4,9 @@ const { employees } = data;
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
-  return employees.find((employee) =>
-    (employees.firstName === employeeName || employees.lastName === employeeName));
+  return employees.find((employee) => employee.firstName === employeeName
+    || employee.lastName === employeeName);
 }
+
 console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
