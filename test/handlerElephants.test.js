323daea67adf213250e44625314898b405775861
Renanbfreitas <renanbf1992@outlook.com>
2022-08-28 14:20:48 -0300

    Adicionando item 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,18 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {});
+it('Retorna a quantidade de elefantes.', () => {
+  const actual = handlerElephants('count');
+  const expected = 4;
+  expect(actual).toBe(expected);
+});
+it('Retorna um array com a relação dos nomes de todos os elefantes', () => {
+  const actual = handlerElephants('names');
+  const expected = 'Jefferson';
+  expect(actual).toContain(expected);
+});
+it('Retorna a média de idade dos elefantes', () => {
+  const actual = handlerElephants('averageAge');
+  const expected = 10.5;
+  expect(actual).toBe(expected);
+});
