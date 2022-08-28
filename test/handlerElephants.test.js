ada3c2c24c87a83e10cf0ebe52ca7bd37473047b
Renanbfreitas <renanbf1992@outlook.com>
2022-08-28 14:32:00 -0300

    Adicionando item 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -16,3 +16,23 @@ it('Retorna a média de idade dos elefantes', () => {
   const expected = 10.5;
   expect(actual).toBe(expected);
 });
+it('Retorna a localização dos elefantes dentro do zoológico', () => {
+  const actual = handlerElephants('location');
+  const expected = 'NW';
+  expect(actual).toBe(expected);
+});
+it('Retorna a popularidade dos elefantes', () => {
+  const actual = handlerElephants('popularity');
+  const expected = 5;
+  expect(actual).toBe(expected);
+});
+it('Retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+  const actual = handlerElephants('availability');
+  const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+  expect(actual).toEqual(expected);
+});
+it('Não passando argumentos a função deve retornar undefined', () => {
+  const actual = handlerElephants();
+  const expected = undefined;
+  expect(actual).toBe(expected);
+});
