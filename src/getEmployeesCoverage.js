18d456556f563f5e4081cb11cbdeb88157baf65b
Renanbfreitas <renanbf1992@outlook.com>
2022-08-27 21:55:34 -0300

    Adicionando item 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+const arrResult = employees.map((objeto) => {
+  const arr = {
+    id: objeto.id,
+    fullName: `${objeto.firstName} ${objeto.lastName}`,
+    species: objeto.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).name),
+    locations: objeto.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).location),
+  };
+  return arr;
+});
+
+function getEmployeesCoverage(prt) {
+  if (prt === undefined) return arrResult;
+  const open = arrResult
+    .find((element) => element.fullName.includes(Object.values(prt))
+      || element.id.includes(Object.values(prt)));
+  if (open === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return open;
 }
 
 module.exports = getEmployeesCoverage;
