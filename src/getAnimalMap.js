8eb70dcce909bcba6244d0a5465e986535a1462b
Renanbfreitas <renanbf1992@outlook.com>
2022-12-25 14:25:14 -0300

    refatorando projeto

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const arrayLocal = ['NE', 'NW', 'SE', 'SW'];
+const objetoGeral = () => arrayLocal.reduce((acc, element) => {
+  acc[element] = species.filter((animal) => animal.location === element).map((elem) => elem.name);
+  return acc;
+}, {});
+
+// console.log(objetoGeral());
+
 function getAnimalMap(options) {
-  // seu código aqui
+  return objetoGeral();
 }
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
